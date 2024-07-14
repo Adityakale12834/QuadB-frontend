da4ad0969d92c6fcce75bbad966aa5f14f82aa58
Adityakale12834 <co.2021.askale@bitwardha.ac.in>
2024-07-14 19:15:41 +0530

    ci/cd Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="h-screen w-screen bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500 flex flex-column items-center px-3">
       <h1 className="text-4xl font-bold my-4">Add Todo</h1>
+      <h1>Hello from aditya</h1>
       <TaskInput/>
       <TaskList/>
     </div>
